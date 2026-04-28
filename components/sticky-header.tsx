1fb2aa45cf1df4d5d088e77e5f606573f6b8d2d3
Massimo Mantineo <mantineomassimo2003@gmail.com>
2026-04-28 16:26:10 +0200

    style: update piazzadellarte brand gradient colors to slightly darker shades

--- components/sticky-header.tsx
+++ components/sticky-header.tsx
@@ -28,7 +28,7 @@ export function StickyHeader({ isLoggedIn = false }: { isLoggedIn?: boolean }) {
             brand === "matricole" ? "bg-[#f4b716]" :
                 brand === "scipog" ? "bg-[#fbc363]" :
                     brand === "dicam" ? "bg-[#f34ab4]" :
-                        brand === "piazzadellarte" ? "bg-[linear-gradient(45deg,#14C5D9_0%,#2EA662_50%,#F29829_100%)]" :
+                        brand === "piazzadellarte" ? "bg-[linear-gradient(45deg,#109fae_0%,#22824b_50%,#c4791d_100%)]" :
                             "bg-[linear-gradient(45deg,#c9041a_0%,#18182e_50%)]"
 
     // Mapping per i loghi e nomi del network
